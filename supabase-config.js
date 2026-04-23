a72f4fbcd1ff8590ef48adde9abd63d672aaeee1
juanv-musa <jvargascorral@gmail.com>
2026-04-23 14:12:21 +0200

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+// Configuración de Supabase
+// Rellena estos campos con los datos de tu proyecto en Supabase
+var SUPABASE_CONFIG = {
+    url: "https://lnlsvpwdycxziihefclh.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxubHN2cHdkeWN4emlpaGVmY2xoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5MTg3OTIsImV4cCI6MjA5MjQ5NDc5Mn0.ecLRvGEDK4Eik2rv-TnvYssEW5DdL4rgWRQ96qKBBTA"
+};
+window.SUPABASE_CONFIG = SUPABASE_CONFIG;
